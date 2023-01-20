2522f28d871f18a59640a8bd4b8e1767110505df
Muhammad-Sharif-Ullah <sharifnkbd@gmail.com>
2023-01-21 00:44:17 +0600

    Initial Commit

---
+++ app/src/main/java/com/sharif/facebookclone/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.sharif.facebookclone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+/// My Application Color Pallet
+
+val PrimaryC1 = Color(0xFF384CFF)
+val PrimaryC2 = Color(0xFF00A3FF)
+val PrimaryGC1 = Color(0xFF384CFF)
+val PrimaryGC2 = Color(0xFF00A3FF)
+val NotificationC1 = Color(0xFFFF0000)
+val NotificationC2 = Color(0xFF36FF00)
+
